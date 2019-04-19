24dbf1f825479d531257ec515304fde4bb5d9b27
TheGaff87 <mgaff87@gmail.com>
2019-04-19 17:27:05 -0400

    Update contact-form.js

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {reduxForm, Field} from 'redux-form';
 
 export default function ContactForm() {
     return (
-        <form>
+        <form onSubmit={props.handleSubmit(values =>
+                    onSubmit(values)>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" name="name" />
+            <Field name="name" id="name" type="text" component="input" />
             <label htmlFor="email">Email address</label>
-            <input type="email" id="email" name="email" />
+            <Field name="email" id="email" type="email" component="input" />
             <label htmlFor="message">Message</label>
-            <textarea id="message" name="message" />
+            <Field name="message" id="message" component="textarea" />
             <button>Submit</button>
         </form>
     );
 }
+
+export default reduxForm({
+    form: 'contact'
+})(ContactForm);
